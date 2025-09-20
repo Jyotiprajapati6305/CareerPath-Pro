d289715accc21e077f179b3138c164bc4fbfdf4d
Jyotiprajapati6305 <118371617+Jyotiprajapati6305@users.noreply.github.com>
2025-09-20 22:43:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -570,4 +570,5 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         print("Tables should now be created:", db.inspect(db.engine).get_table_names())
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
